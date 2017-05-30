e73dcfa061553710695c5d72a70c7ab62c6b0eab
shunya-mandal <100ameya@gmail.com>
2017-05-30 18:11:10 +0000

    Done

--- build.py
+++ build.py
@@ -2,13 +2,22 @@ import operator
 
 
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
+    """
+    Sorts given dictionary by keys in ascending order.
+
+    Returns tuple of sorted (value, key)
+    """
+    return sorted(dic.items(), key=operator.itemgetter(0))
 
 
 def solution_desc(dic):
-    '''
-    Enter your code here
-    '''
+    """
+    Sorts given dictionary by values in descending order.
 
+    Returns tuple of sorted (value, key)
+    """
+    return sorted(dic.items(), key=operator.itemgetter(0), reverse=True)
+
+
+d = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+print(solution_asc(d))
